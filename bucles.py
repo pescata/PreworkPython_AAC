f5f3360184a8dd0f32d7ba17e0d5347f4c7a9208
pescata <aalvarez_cid@hotmail.com>
2023-07-16 01:44:40 +0200

    feat: ejercicios bucles completados

--- bucles.py
+++ bucles.py
@@ -1,8 +1,7 @@
 '''
 1. Ejercicio: Imprime los números del 1 al 10 usando un bucle for.
 '''
-numbers=(1,2,3,4,5,6,7,8,9,10)
-for number in numbers:
+for number in range(1,11):
     print(number)
 
 '''
@@ -10,8 +9,8 @@ for number in numbers:
 '''
 i=2
 while i<=20:
-   print(i)
-i+=2
+    print(i)
+    i+=2
 
 '''
 3. Ejercicio: Usa un bucle para calcular la suma de los números del 1 al 100.
