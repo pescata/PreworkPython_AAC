018bf5fb8268aa7ec1d17e171fcc6f6e201bfa83
pescata <aalvarez_cid@hotmail.com>
2023-07-18 02:35:38 +0200

    feat: updated 18/07/023

--- 5_bucles y funciones/06.byf.py
+++ 5_bucles y funciones/06.byf.py
@@ -8,6 +8,9 @@ print("Ejercicio_6: función que devuelve los primeros n elementos de una lista"
 n=int(input("introduce el número de elementos: "))
 
 def primeros_n_elementos(lista, n):
-  return lista[:n]
-print(f"los primeros {n} elementos de la lista son: ",primeros_n_elementos([1, 2, 3, 4, 5, 6, 7], 4))
+  if n>len(lista):
+    print("el número indicado supera el número de elementos de la lista")
+  else:
+    return lista[:n]
+print(f"los primeros {n} elementos de la lista son: ",primeros_n_elementos([1, 2, 3, 4, 5, 6, 7], n))
 
