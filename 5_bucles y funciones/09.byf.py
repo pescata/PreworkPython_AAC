90c59cb857306b5351a8d6e2601d81279cd2117e
pescata <aalvarez_cid@hotmail.com>
2023-07-18 13:58:07 +0200

    feat: updated 18/07/2023

--- 5_bucles y funciones/09.byf.py
+++ 5_bucles y funciones/09.byf.py
@@ -4,3 +4,14 @@ representación en binario.
 '''
 print("Ejercicio_9: función para conversión de número decimal a binario")
 
+n=int(input("introduce el numero que quieres convertir a binario: "))
+def DecBin(n):
+    d=[]
+    print(f"el número {n}",end=" ")
+    while n>=1:
+        d.append(n%2)
+        n=n//2
+    s=d[::-1]
+    print("se corresponde con el binario:",s,end=" ")
+
+DecBin(n)
--- 5_bucles y funciones/10.byf.py
+++ 5_bucles y funciones/10.byf.py
@@ -2,3 +2,15 @@
 10. Ejercicio: Define una función que reciba dos listas y retorne la intersección de
 ambas (los elementos que están en las dos listas).
 '''
+print("Ejercicio_10: función intersección listas")
+lista_1=input("introduce los elementos de la lista_1 separados por espacios: ").split()
+lista_2=input("introduce los elementos de la lista_2 separados por espacios: ").split()
+
+print("lista_1: ",lista_1)
+print("lista_2: ",lista_2)
+
+def interseccion(lista_1,lista_2):
+    return list(set(lista_1) & set(lista_2))
+print ("lista_intersección: ",interseccion (lista_1,lista_2))
+
+interseccion(lista_1,lista_2)
