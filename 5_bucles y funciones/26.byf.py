f945c41630cf959768ab7e14cae32701262e6d3a
pescata <aalvarez_cid@hotmail.com>
2023-07-26 21:25:17 +0200

    feat: updated 26/07/2023

--- 5_bucles y funciones/26.byf.py
+++ 5_bucles y funciones/26.byf.py
@@ -2,4 +2,13 @@
 26. Ejercicio: Define una función que tome dos listas y retorne la lista de
 elementos que no están en ambas listas.
 '''
+lista_1=[1,2,3,4,5,6,7,8,9]
+lista_2=[2,4,6,8,9]
+print("lista_1: ",lista_1)
+print("lista_2: ",lista_2)
 
+def eltos_distintos (lista_1,lista_2):
+    return list(set(lista_1) ^ set(lista_2))
+print("la lista de elementos distintos es: ",(eltos_distintos(lista_1,lista_2)))
+
+eltos_distintos (lista_1,lista_2)
--- 5_bucles y funciones/31.byf.py
+++ 5_bucles y funciones/31.byf.py
@@ -2,4 +2,26 @@
 31. Ejercicio: Define una función que reciba un número entero n y retorne una lista
 con los n primeros números primos.
 '''
+print("Ejercicio_31: función que recibe un número n y retorna lista con los n primeros números primos")
 
+n=int(input("introduzca un número: "))
+
+def num_primo(n):
+    if n<=1:
+       return False
+    for i in range(2,n*n+1):
+            if n%i == 0:
+                return False
+            else:
+               return True
+
+def primeros_n_primos(n):
+   primos = []
+   numero = 2
+   while len(primos) < n:
+      if num_primo(numero):
+         primos.append(numero)
+      numero += 1
+   print(f"los {n} primeros números primos son: ",primos)
+
+primeros_n_primos(n)
